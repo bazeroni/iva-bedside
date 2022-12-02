50765790d9987f408b01f619ca8871d2189f00ee
Sebastian <44552816+bazeroni@users.noreply.github.com>
2022-12-01 20:39:42 -0500

    add bedside variables

---
+++ scratch.py
@@ -0,0 +1,77 @@
+# sets up identifiers for conversation
+bot = "VBA"
+patient = "Andrew Lopez"
+
+### bedside variables ###
+
+## basic info
+dob = "03-22-95"
+age = "27"
+weight = "63 kg"
+height = "180.3 cm"
+
+dateCurrent = "11-30-22"
+timeCurrent = "12:23 AM"
+zone = "Rock Pavilion (Zone A)"
+floor = "3 West"
+roomNumber = "A315"
+roomTemperature = "73.4 F"
+dateAdmitted = "11-22-22"
+dateDischarge = "12-10-22"
+
+## vitals
+bloodPressure = "125/90 mmHg"
+pulse = "77 bpm"
+temperature = "97.9 F"
+respiratoryRate = "16 breaths/min"
+oxygenSaturation = "96%"
+
+## conditions and precautions
+lastPainMedRequest = "11-29-22 07:15 AM"
+mobility = "Bed Rest"
+restrictFluid = "Yes, please see nurse"
+diet = "Nothing by mouth after midnight"
+fallRisk = "Yes, call for assistance"
+isolation = "Yes, visitors must follow isolation instructions"
+
+## allergies
+allergies = "-Peanut\n-Shellfish\n-Penicillin"
+
+## medications
+medications = "-Amoxicillin\n-Ibuprofen"
+
+## reason for admission
+reasonForAdmission = "Suspected pneumonia"
+
+## assessment
+assessment = "-Chest X-ray shows infiltrates in the lower lobes of the lungs\n-Cough with yellow sputum production\n-Shortness of breath"
+
+## treatment plan
+treatmentPlan = "-Administer intravenous antibiotics\n-Administer oxygen therapy\n-Monitor vital signs and oxygen saturation levels\n-Encourage deep breathing and coughing exercises"
+
+## follow-up
+followUp = "-Repeat chest X-ray in 48 hours\n-Consult with pulmonologist for further management"
+
+## discharge plan
+dischargePlan = "-Prescribe oral antibiotics for 7-10 days\n-Follow up with primary care physician in 1 week\n-Recommend influenza vaccine to prevent future respiratory infections"
+
+## care team
+attendingProvider = "Dr. Jacksonville"
+pulmonologist = "Dr. Washington"
+respiratoryTherapist = "Sandra"
+physicalTherapist = "Mary"
+nursePractitioner = "Dietra"
+registeredNurse = "Maureen"
+nurseAssistant = "David"
+
+## agenda
+goals = "-Rest\n-Coughing Exercises\n-Family Meeting AM"
+events = "-Morning: Radiology\n-Afternoon: Pulmonologist"
+consults = "-West Chester Radiology"
+dailyMessage = "\"Hey this is RN Jane. Great job today! See you tomorrow\""
+
+## Compiled Chart ##
+
+chart = f"Patient: {patient}\nDate of Birth: {dob}\nAge: {age}\nWeight: {weight}\nHeight: {height}\n\nCurrent Date: {dateCurrent}\nCurrent Time: {timeCurrent}\nZone: {zone}\nFloor: {floor}\nRoom: {roomNumber}\nRoom Temperature: {roomTemperature}\nDate Admitted: {dateAdmitted}\nExpected Discharge Date: {dateDischarge}\n\nALLERGIES\n{allergies}\n\nMEDICATIONS\n{medications}\n\nREASON FOR ADMISSION\n{reasonForAdmission}\n\nASSESSMENT\n{assessment}\n\nTREATMENT PLAN\n{treatmentPlan}\n\nFOLLOW-UP\n{followUp}\n\nDISCHARGE PLAN\n{dischargePlan}\n\nCONDITIONS AND PRECAUTIONS\nLast Pain Med Request: {lastPainMedRequest}\nMobility: {mobility}\nFluid Restriction: {restrictFluid}\nDiet: {diet}\nFall Risk: {fallRisk}\nIsolation: {isolation}\n\nVITALS\nBP: {bloodPressure}\nHR: {pulse}\nRR: {respiratoryRate}\nTemp: {temperature}\nO2: {oxygenSaturation}\n\nCARE TEAM\nAttending Provider: {attendingProvider}\nPulmonologist: {pulmonologist}\nRespiratory Therapist: {respiratoryTherapist}\nPhysical Therapist: {physicalTherapist}\nNurse Practitioner: {nursePractitioner}\nRegistered Nurse: {registeredNurse}\nNurse Assistant: {nurseAssistant}\n\nGOALS / PLAN FOR THE DAY\n{goals}\n\nUPCOMING EVENTS\n{events}\n\nUPCOMING CONSULTS\n{consults}\n\nPREVIOUS SHIFT MESSAGE TO PATIENT\n{dailyMessage}\n\n----------------START OF CHAT-------------------\n"
+
+print(chart)
--- synia-s2s.py
+++ synia-s2s.py
@@ -1,30 +1,12 @@
-#from curses import echo
-from email import message
-from tkinter.ttk import Style
-from unittest import result
-from urllib import response
-from urllib.parse import uses_query
-from matplotlib.animation import Animation
-from matplotlib.pyplot import text
-import nltk
-from nltk.stem import LancasterStemmer
-import numpy as np
-import pickle
-import random
-import json
+#import random
+#import keyboard
+#import whisper
+#import io
+#import time
 import os
-import time
 import openai
 from dotenv import load_dotenv
 import azure.cognitiveservices.speech as speechsdk
-import xml.etree.ElementTree as ET
-import xmltodict
-import tempfile
-#import animation
-#import whisper
-import speech_recognition as sr
-#from pydub import AudioSegment
-import io
 from playsound import playsound
 
 # loads env variables file
@@ -43,22 +25,15 @@ speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY, region="ea
 
 # configs stt lang
 speech_config.speech_recognition_language="en-US"
-#speech_config.speech_recognition_language="ko-KR"
-#speech_config.speech_recognition_language="fil-PH"
-#speech_config.speech_recognition_language="fr-CA"
 #speech_config.speech_recognition_language="es-US"
-#speech_config.speech_recognition_language="fa-IR"
 
 # configs tts voice
 # other than Aria, style compatible (-empathetic) with Davis, Guy, Jane, Jason, Jenny, Nancy, Tony
 
 speech_config.speech_synthesis_voice_name='en-US-JennyNeural'
-#speech_config.speech_synthesis_voice_name='ko-KR-SunHiNeural'
+#speech_config.speech_synthesis_voice_name='en-US-JennyMultilingualNeural'
 #speech_config.speech_synthesis_voice_name='en-US-AIGenerate1Neural'
-#speech_config.speech_synthesis_voice_name = 'zh-CN-XiaomoNeural'
 #speech_config.speech_synthesis_voice_name='es-MX-CarlotaNeural'
-#speech_config.speech_synthesis_voice_name = 'fil-PH-AngeloNeural'
-#speech_config.speech_synthesis_voice_name = 'fil-PH-BlessicaNeural'
 
 # sets voice
 voice = speech_config.speech_synthesis_voice_name
@@ -77,8 +52,80 @@ speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audi
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=tts_config)
 
 # sets up identifiers for conversation
-user = "Bash"
-bot = "Jenny"
+bot = "VBA"
+patient = "Andrew Lopez"
+
+### bedside variables ###
+
+## basic info
+dob = "03-22-95"
+age = "27"
+weight = "63 kg"
+height = "180.3 cm"
+
+dateCurrent = "11-30-22"
+timeCurrent = "12:23 AM"
+zone = "Rock Pavilion (Zone A)"
+floor = "3 West"
+roomNumber = "A315"
+roomTemperature = "73.4 F"
+dateAdmitted = "11-22-22"
+dateDischarge = "12-10-22"
+
+## vitals
+bloodPressure = "125/90 mmHg"
+pulse = "77 bpm"
+temperature = "97.9 F"
+respiratoryRate = "16 breaths/min"
+oxygenSaturation = "96%"
+
+## conditions and precautions
+lastPainMedRequest = "11-29-22 07:15 AM"
+mobility = "Bed Rest"
+restrictFluid = "Yes, please see nurse"
+diet = "Nothing by mouth after midnight"
+fallRisk = "Yes, call for assistance"
+isolation = "Yes, visitors must follow isolation instructions"
+
+## allergies
+allergies = "-Peanut\n-Shellfish\n-Penicillin"
+
+## medications
+medications = "-Amoxicillin\n-Ibuprofen"
+
+## reason for admission
+reasonForAdmission = "-Suspected pneumonia"
+
+## assessment
+assessment = "-Chest X-ray shows infiltrates in the lower lobes of the lungs\n-Cough with yellow sputum production\n-Shortness of breath"
+
+## treatment plan
+treatmentPlan = "-Administer intravenous antibiotics\n-Administer oxygen therapy\n-Monitor vital signs and oxygen saturation levels\n-Encourage deep breathing and coughing exercises"
+
+## follow-up
+followUp = "-Repeat chest X-ray in 48 hours\n-Consult with pulmonologist for further management"
+
+## discharge plan
+dischargePlan = "-Prescribe oral antibiotics for 7-10 days\n-Follow up with primary care physician in 1 week\n-Recommend influenza vaccine to prevent future respiratory infections"
+
+## care team
+attendingProvider = "Dr. Jacksonville"
+pulmonologist = "Dr. Washington"
+respiratoryTherapist = "Sandra"
+physicalTherapist = "Mary"
+nursePractitioner = "Dietra"
+registeredNurse = "Maureen"
+nurseAssistant = "David"
+
+## agenda
+goals = "-Rest\n-Coughing Exercises\n-Family Meeting AM"
+events = "Morning: Radiology\nAfternoon: Pulmonologist"
+consults = "-West Chester Radiology"
+dailyMessage = "-\"Hey this is RN Jane. Great job today! See you tomorrow\""
+
+## Compiled Chart ##
+
+chart = f"Patient: {patient}\nDate of Birth: {dob}\nAge: {age}\nWeight: {weight}\nHeight: {height}\n\nCurrent Date: {dateCurrent}\nCurrent Time: {timeCurrent}\nZone: {zone}\nFloor: {floor}\nRoom: {roomNumber}\nRoom Temperature: {roomTemperature}\nDate Admitted: {dateAdmitted}\nExpected Discharge Date: {dateDischarge}\n\nALLERGIES\n{allergies}\n\nMEDICATIONS\n{medications}\n\nREASON FOR ADMISSION\n{reasonForAdmission}\n\nASSESSMENT\n{assessment}\n\nTREATMENT PLAN\n{treatmentPlan}\n\nFOLLOW-UP\n{followUp}\n\nDISCHARGE PLAN\n{dischargePlan}\n\nCONDITIONS AND PRECAUTIONS\nLast Pain Med Request: {lastPainMedRequest}\nMobility: {mobility}\nFluid Restriction: {restrictFluid}\nDiet: {diet}\nFall Risk: {fallRisk}\nIsolation: {isolation}\n\nVITALS\nBP: {bloodPressure}\nHR: {pulse}\nRR: {respiratoryRate}\nTemp: {temperature}\nO2: {oxygenSaturation}\n\nCARE TEAM\nAttending Provider: {attendingProvider}\nPulmonologist: {pulmonologist}\nRespiratory Therapist: {respiratoryTherapist}\nPhysical Therapist: {physicalTherapist}\nNurse Practitioner: {nursePractitioner}\nRegistered Nurse: {registeredNurse}\nNurse Assistant: {nurseAssistant}\n\nGOALS / PLAN FOR THE DAY\n{goals}\n\nUPCOMING EVENTS\n{events}\n\nUPCOMING CONSULTS\n{consults}\n\nPREVIOUS SHIFT MESSAGES TO PATIENT\n{dailyMessage}\n\n----------------START OF CHAT-------------------\n"
 
 ### SETUP VARIABLES ###
 # concats message history for re-insertion with every prompt
@@ -89,29 +136,39 @@ raspuns = ""
 raspunsF = ""
 # holds emotional response chosen by GPT-3
 style = ""
-# counts number of times user silence for input
+# counts number of times patient silence for input
 silenceCount = 0
 # counts number of messages in conversation history 
 messageCount = 0
 
 """ TONE_GPT3()
-1. inputs and reads user prompt
+1. inputs and reads patient prompt
 3. chooses emotional response from given list of styles
 2. returns style/emotion
 """
 def tone_gpt3(zice):
     toneLabel = openai.Completion.create(
         engine="text-davinci-003",
-        #prompt="Read the following interaction, then pick just one of the emotions for "+bot+" to respond to "+user+" with from this list only: [unfriendly, angry, angry, shouting, shouting, sad, terrified, whispering, whispering, whispering, hopeful, cheerful, excited, friendly].\n"+bot+": "+raspuns+"\n"+user+": "+zice+"\n\nEmotion: [",
-        prompt="Read the following interaction, then pick just one of the emotions for "+bot+" to respond to "+user+" with from this list only: [friendly, empathetic, cheerful, excited, hopeful, unfriendly, angry, shouting, sad, terrified, whispering].\n"+bot+": "+raspuns+"\n"+user+": "+zice+"\n\nEmotion: [",
+        prompt="Read the following interaction, then pick just one of the emotions for "+bot+" to respond to "+patient+" with from this list only: [friendly, empathetic, excited, hopeful, sad].\n"+bot+": "+raspuns+"\n"+patient+": "+zice+"\n\nEmotion: [",
         temperature=0.0,
         max_tokens=12,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=[user+":", bot+":", "Emotion: [", ","],
+        stop=[patient+":", bot+":", "Emotion: [", ","],
+        echo=False,
     )
     return toneLabel
+
+# Define a callback function that will be called
+# whenever a key is pressed
+def on_key_press(key):
+    # Print the character that was pressed
+    print("You pressed: ", key.name)
+
+    input = input(patient + ": ")
+
+    textSpeech(input)
 
 def concatContext():
     
@@ -121,28 +178,27 @@ def concatContext():
     if len(messages) == 6:
         messages.pop()
         
-    print(len(messages))
+    #print(len(messages))
         
     for message in messages:
         context += message
 
 """ CHAT_GPT3()
-1. inputs and reads user prompt
+1. inputs and reads patient prompt
 2. responds with given style from TONE_GPT3()
 3. returns response
 """
 def chat_gpt3(zice):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        #prompt= bot+" is helping "+user+" speak Spanish. "+context+"\n"+user+": "+zice+"\n"+bot+":",
-        prompt= "This is a chat between "+bot+" and "+user+". You are "+bot+". "+bot+" loves to have interesting and random conversations. "+bot+" is also very funny and witty. "+bot+" speaks informally and casually, constantly using filler words such as 'um', 'uh', 'like', 'very', 'really', 'just', 'you know', 'i mean', 'i guess', 'totally', 'literally', 'seriously', and others when speaking."+context+"\n"+user+": "+zice+"\n"+bot+" ["+style+"]:",
+        prompt= "You are, "+bot+", a virtual bedside assistant at Temple University Hospital for a patient named "+patient+". You are comforting, assuring, empathetic, and kind. Read the patient's chart below and answer any questions that may arise. If nurse intervention is required, kindly instruct the patient to press their nurse call button.\n\n"+chart+context+"\n"+patient+": "+zice+"\n"+bot+" ["+style+"]:",
         temperature=1.0,
         max_tokens=256,
         top_p=1.0,
-        frequency_penalty=1.0,
+        frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=[user+":", bot+":", "["],
-        echo=False,
+        stop=[patient+":", bot+":", "["],
+        echo=True,
     )
     return response
 
@@ -152,45 +208,8 @@ def chat_gpt3(zice):
 """
 def tts(ssml):
     global speech_synthesis_result
-    #speech_recognizer.stop_speaking_async()
     speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml).get()
-    #speech_synthesis_result = speech_synthesizer.start_speaking_ssml_async(ssml)
-"""
-def stt(model="base", english=False, verbose=False, energy=300, pause=0.5, dynamic=True):
-    
-    temp_dir = tempfile.mkdtemp()
-    save_path = os.path.join(temp_dir, "temp.wav")
 
-    audio_model = whisper.load_model(model)
-    
-    r = sr.Recognizer()
-    r.energy_threshold = energy
-    r.pause_threshold = pause
-    r.dynamic_energy_threshold = dynamic
-
-    with sr.Microphone(sample_rate=16000) as source:
-            
-        # prints status
-        print("|||||||||| LISTENING ||||||||||")
-        
-        audio = r.listen(source)
-        data = io.BytesIO(audio.get_wav_data())
-        audio_clip = AudioSegment.from_file(data)
-        audio_clip.export(save_path, format="wav")
-
-        if english:
-            result = audio_model.transcribe(save_path,language='english')
-        else:
-            result = audio_model.transcribe(save_path)
-
-        if not verbose:
-            predicted_text = result["text"]
-            print("You said: " + predicted_text)
-        else:
-            print(result)
-    
-    return predicted_text
-"""
 while (True):
 
     try:
@@ -205,8 +224,8 @@ while (True):
             
             inp.encode("utf-8")
             
-            # parses and formats user input
-            prompt = user+": "+inp
+            # parses and formats patient input
+            prompt = patient+": "+inp
 
             # global counter and helper variables
             global silenceCount
@@ -271,11 +290,11 @@ while (True):
             # processes silent interaction
             else:
 
-                # checks if user has been silent for certain amount of time
+                # checks if patient has been silent for certain amount of time
                 if silenceCount == 2:
                     
                     # imitates silent input
-                    prompt = user+": ..."
+                    prompt = patient+": ..."
 
                     # prints status
                     #print("SILENCE PROMPT")
@@ -333,28 +352,19 @@ while (True):
         # listens for speech
         while not done:
             
-            print("|||||||||| LISTENING ||||||||||")
-            #playsound('start.mp3', False)
-
-            # prompts for text input
-            #input = input()
-
-            # processes text input
-            #textSpeech(input)
+            print("\n|||||||||| LISTENING ||||||||||\n")
+            playsound('start.mp3', False)
             
             # gets azure stt
             speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
-            #playsound('stop.mp3', False)
+            playsound('stop.mp3', False)
 
             # gets tts from azure stt
             speech_recognizer.recognized.connect(textSpeech(speech_recognition_result.text))
 
-            # gets whisper stt
-            #speech_recognition_result = stt()
-
-            # gets tts from whisper stt
-            #textSpeech(speech_recognition_result)
+            # Start listening for keystrokes
+            #keyboard.on_press(on_key_press)
 
     # excepts if not understood
     except Exception as e:
